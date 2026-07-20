5ae0281d2de1e1078e9f4df44a59e01cb6a8c704
Florian Cramer <94528595+FlorianCramer@users.noreply.github.com>
2026-07-20 22:43:14 +0000

    add app with capacitor

---
+++ app/capacitor.config.ts
@@ -0,0 +1,22 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.opensource.openwarnde',
+  appName: 'OpenWarnDE',
+  webDir: 'out',
+
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: '#EF6F2C',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: 'launch_screen',
+    },
+  },
+};
+
+export default config;
