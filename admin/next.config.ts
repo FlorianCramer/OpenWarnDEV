b7c8bec703bac84f7bbdbe216996500d56d7f8c6
Florian Cramer <94528595+FlorianCramer@users.noreply.github.com>
2026-08-17 10:46:21 +0000

    build admin nextjs initial

--- admin/next.config.ts
+++ admin/next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  allowedDevOrigins: ['127.0.0.1']
+  /* config options here */
 };
 
 export default nextConfig;
